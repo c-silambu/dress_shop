1eeba9d90e1801c8c0b87f75f1f0a4cba48e5aea
c-silambu <144410442+c-silambu@users.noreply.github.com>
2026-07-20 17:58:20 +0530

    Fix email delivery

--- backend/server.js
+++ backend/server.js
@@ -12,7 +12,7 @@ const app = express();
 connectDB();
 
 app.use(helmet({ crossOriginResourcePolicy: false }));
-app.use(cors({ origin: process.env.FRONTEND_URL || "*", credentials: true }));
+app.use(cors({ origin:"http://localhost:5173"||process.env.FRONTEND_URL || "*", credentials: true }));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(morgan("dev"));
