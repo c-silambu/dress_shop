d566e158783e8470b6f5e8d6fac991068a03ed22
c-silambu <144410442+c-silambu@users.noreply.github.com>
2026-07-21 13:24:38 +0530

    chnageing server js

--- backend/server.js
+++ backend/server.js
@@ -12,7 +12,11 @@ const app = express();
 connectDB();
 
 app.use(helmet({ crossOriginResourcePolicy: false }));
-app.use(cors({ origin:"http://localhost:5173"||process.env.FRONTEND_URL || "*", credentials: true }));
+app.use(cors({   origin: [
+    "http://localhost:5173",
+    "https://dress-shop-wm4n.vercel.app"
+  ]
+  , credentials: true }));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(morgan("dev"));
